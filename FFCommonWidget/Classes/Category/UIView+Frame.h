bce9c758232a555c0bc8a1b6516f0b420d549f88
nonoking <nonoking@nonokingdeMacBook-Pro.local>
2017-10-22 23:36:16 +0800

    测试一下

---
+++ FFCommonWidget/Classes/Category/UIView+Frame.h
@@ -0,0 +1,20 @@
+//
+//  UIView+Frame.h
+//  GaGa
+//
+//  Created by nonoking on 2017/9/27.
+//  Copyright © 2017年 aiyesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Frame)
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+@property (nonatomic, assign) CGPoint origin;
+@property (nonatomic, assign) CGSize  size;
+@end
